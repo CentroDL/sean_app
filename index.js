1ff1699c83b16a5d09f585ed3894dcf38d584922
Jaden Carver <jaden.carver@gmail.com>
2016-01-18 21:01:32 -0500

    updates code

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ let accessDatabase = postgresql({
     config: {
       database: "sean_app_dev",
     },
-    // transaction : true
+    transaction : true
   });
 
 routes()
   .use(morgan('combined'))
   .get("/", accessDatabase, async (req, res) => {
-    // const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+    let sleep = (ms = 0) => {
+      return new Promise(r => setTimeout(r, ms));
+    };
     let sql = (sql) => {
       return new Promise((resolve, reject) => {
         req.db.client.query(sql, (err, result) => {
           if (err) { reject(err) } else { resolve(result.rows) }
         });
       });
-    };
-    let sleep = (ms = 0) => {
-      return new Promise(r => setTimeout(r, ms));
     };
 
     console.log("start");
