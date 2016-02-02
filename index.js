b139add9a821b96bcc55dfa558bfb0ca209daa19
Jaden Carver <jaden.carver@gmail.com>
2016-02-02 14:22:49 -0500

    attempts to use pg-promise

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 let routes = require("express");
-let postgresql = require('connect-pgclient');
 let morgan = require('morgan');
+let pgp = require('pg-promise')();
 
-let accessDatabase = postgresql({
-    config: {
-      database: "sean_app_dev",
-    },
-    transaction : true
-  });
+var cn = {
+    host: 'localhost', // server name or IP address;
+    port: 5432,
+    database: 'sean_app_dev',
+    user: 'jaden',
+    password: null
+};
+
+var db = pgp(cn);
 
 routes()
   .use(morgan('combined'))
-  .get("/", accessDatabase, async (req, res) => {
+  .get("/", async (req, res) => {
     let sleep = (ms = 0) => {
       return new Promise(r => setTimeout(r, ms));
     };
     let sql = (sql) => {
-      return new Promise((resolve, reject) => {
-        req.db.client.query(sql, (err, result) => {
-          if (err) { reject(err) }
-          else { resolve(result) }
-        });
-      });
+      return db.query(sql);
     };
 
     console.log("start");
     await sleep(1000);
     console.log("wake up");
     // sql("SELECT * FROM users").then(result => {
-    //   console.log(".then works: ", result.rows);
-    //   res.send(result.rows);
+    //   console.log(".then works: ", result);
+    //   res.send(result);
     // })
     // TODO: FOR SOME REASON THIS AWAIT DOESN'T WORK, EVEN THOUGH THE PROMISE
     //       ABOVE WORKS!
     result = await sql("SELECT * FROM users");
     console.log("await works");
-    res.send(result.rows);
+    res.send(result);
     console.log("done");
   })
   .listen(3000);
